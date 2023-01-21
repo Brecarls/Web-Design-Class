119a37114cf51302b703f91fd7188294f0b286a5
Brendan Carlson <122575218+Brecarls@users.noreply.github.com>
2023-01-21 11:23:54 -0700

    Add files via upload

---
+++ helper.js
@@ -0,0 +1,13 @@
+
+function displayText(){
+    document.getElementById("mytext").innerHTML = "My majors are AI and ACS! And I graduate May 2023!";
+}
+
+function displayText2(){
+    document.getElementById("mytext2").innerHTML = "If by any chance you need to contact me please use the email below.";
+}
+
+function displayText3(){
+    document.getElementById("mytext3").innerHTML = "brecarls@uat.edu";
+}
+
